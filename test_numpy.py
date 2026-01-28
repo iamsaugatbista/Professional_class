6711fb47e6e727e660acdc48a18f9f2907f9f6bc
iamsaugatbista <iamsaugatbista@gmail.com>
2026-01-28 06:45:42 +0545

    reset project

--- test_numpy.py
+++ test_numpy.py
@@ -1,5 +1,7 @@
+# NUMpy Example
 import numpy as np
-a = np.array([1,2,3]); b = np.array([4,5,6])
-print("Sum:", a+b, "Product:", a*b)
-print("Mean of a:", np.mean(a), "Max of b:", np.max(b))
-c = a.reshape(3,1); print("Reshaped a:\n", c)
+
+arr= np.array([10,20,30,40])
+print("Array:", arr)
+print("Mean:",np.mean(arr))
+print("sum:",np.sum(arr))
